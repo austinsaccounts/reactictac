12da58d74a0cb5408876134b1bbc9313d0ce0689
Austin C <austins.accts@gmail.com>
2019-02-19 01:26:16 -0800

    change square class to a function component

--- src/index.js
+++ src/index.js
@@ -2,31 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value:null,
-        };
-    }
-        
-        render() {
-            return (
-                <button className="square" 
-                onClick={() => this.setState({value:'X'})}
-                >
-                {this.state.value}
-            </button>
-        );
-    }
+// class Square extends React.Component {
+//     render() {
+//         return (
+//             <button className="square" 
+//             onClick={() => this.props.onClick()}
+//             >
+//             {this.props.value}
+//         </button>
+//         );
+//     }
+// }
+//---- aobve is the class form, but the new is a simpler function as it only requries render
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
-
+//booard is controlling the onClicks, board is sending values to squares
+// squares only report being clicked and ask for board help
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i}/>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
     }
 
+    handleClick(i) {
+        //copies the array of squares
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i) {
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+// above, were doing this to track x-o-null, 
+// and board will now maintain what squares filled, broken down in lines for ease of reading
+   
     render() {
         return (
             <div>
